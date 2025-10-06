55ccb441110321825470908965701aa77b89ee0a
TacNap28 <matthewtlittle@gmail.com>
2025-10-06 12:06:55 +1000

    decalred initial fields and methods

--- Grid.cs
+++ Grid.cs
@@ -1,3 +1,5 @@
+using System.ComponentModel;
+
 public class Grid
 {
     // private Disc[][] Board;
@@ -10,21 +12,58 @@ public class Grid
     public int TurnCounter { get; private set; }
 
     // Methods
-    void IncrementTurnCounter()
+    public void IncrementTurnCounter()
     {
         TurnCounter++;
     }
 
-    void DecrementTurnCounter()
+    public void DecrementTurnCounter()
     {
         TurnCounter--;
     }
 
     // Needs to be updated in class diagram
-    bool SetTurnCounter(int num)
+    public bool SetTurnCounter(int num)
     {
         if (num < 0) return false;
         TurnCounter = num;
         return true;
+    }
+
+    // needs Player object parameters
+    // Not implemented yet
+    public bool IsTieGame()
+    {
+        Console.WriteLine("This method isn't implemented yet");
+        return false;
+    }
+
+    public bool AddDisc(int lane)
+    {
+        Console.WriteLine("This method isn't implemented yet");
+        return false;
+    }
+
+    // requires Orientation parameter
+    public void ApplyGravity()
+    {
+        Console.WriteLine("This method isn't implemented yet");
+        return;
+    }
+
+    public void Spin()
+    {
+        return;
+    }
+
+    // requires Orientation parameter
+    public void DrawGrid()
+    {
+        return;
+    }
+
+    public bool CheckWinCondition()
+    {
+        return false;
     }
 }
