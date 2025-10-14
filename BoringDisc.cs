76cb77f0fb4e0af17c7b346086f29598ac086e87
Tyler <trieuhaipham32621998@gmail.com>
2025-10-14 21:53:46 +1000

    [add] [disc] Boring: Constructor, basic effect

--- BoringDisc.cs
+++ BoringDisc.cs
@@ -2,24 +2,37 @@
 
 public class BoringDisc : Disc
 {
-	
-	public override void ApplyEffects(Disc[][] Board, int lane)
+    public BoringDisc(string symbol_) : base(symbol_)
+    {
+    }
+
+    public override void ApplyEffects(Disc?[][] Board, int lane)
 	{
 		Console.WriteLine("[Run]\t BoringDisc, ApplyEffects");
-		// Apply effect
-			// Loop through played lane, check if disc = symbol
-			// Drill down to the bottom of the lane
-			// Return all disk to hands of respective players
-		for (int i = Board.Length - 1; i >= 0; i--)
+		string PlayerDiscSymbol = (IsPlayerOne) ? "@" : "#";
+		int DiscCount1 = 0;
+		int DiscCount2 = 0;
+		
+		// Loop through played lane from the top
+		for (int i = 0; i < Board.Length; i++)
 		{
 			Disc? d = Board[i][lane];
-			if (d == null) continue;
+			if (d.Symbol == "@") DiscCount1 += 1;
+			if (d.Symbol == "#") DiscCount2 += 1;
+
+			// Check if disc = symbol
 			if (d.Symbol == this.Symbol)
 			{
+				// Drill down to the bottom of the lane and Convert special disc into ordinary
+				Disc NewDisc = new OrdinaryDisc(PlayerDiscSymbol);
+				Disc?[] NewLane = new Disc[Board.Length];
+				NewLane[0] = NewDisc;
+				Board[i] = NewLane;
 
+				// Return all disk to hands of respective players
+				// ...
 			}
 		}
-		// Convert special disc into ordinary
 		
 		// Return (?)
 	}
