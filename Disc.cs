ccfa14e0c85e5bac4e14b88033381ad9890784ab
Tyler <trieuhaipham32621998@gmail.com>
2025-10-14 21:52:33 +1000

    [add] [disc] Constructor

--- Disc.cs
+++ Disc.cs
@@ -6,8 +6,12 @@ using System.Dynamic;
 public class Disc
 {
 	public string Symbol { get; protected set; }
-
 	public bool IsPlayerOne { get; protected set; }
+	
+	public Disc(string symbol_)
+    {
+		Symbol = symbol_;
+    }
 
 	public virtual void ApplyEffects(Disc[][] Board, int lane)
 	{
