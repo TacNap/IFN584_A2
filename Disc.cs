afe9e733521f9fe69c9d355a8543021dc8e403ee
Trieu-Tyler <70167987+meowliodaz@users.noreply.github.com>
2025-10-15 21:40:02 +1000

    Revert "Tyler disc effect"

--- Disc.cs
+++
@@ -1,9 +0,0 @@
-
-
-public abstract class Disc
-{
-	public string Symbol { get; protected set; }
-	public bool IsPlayerOne { get; protected set; }
-
-	public abstract bool ApplyEffects(ref Disc?[][] Board, int lane);
-}
