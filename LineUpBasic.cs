5dab32d1acd788a4faf269575bcff012713651e1
TacNap28 <matthewtlittle@gmail.com>
2025-10-15 18:34:05 +1000

    updated GameLoop to differentiate Player objects - NOT IDEAL

--- LineUpBasic.cs
+++ LineUpBasic.cs
@@ -26,6 +26,7 @@ public class LineUpBasic : Game {
         file = new FileController();
     }
 
+    // This can just be a parent method 
     public override string GetInputGame()
     {
         Console.WriteLine("Enter move/command");
@@ -34,15 +35,47 @@ public class LineUpBasic : Game {
         return input.ToLower();
     }
 
+    /// <summary>
+    /// Check if the input describes a valid move for this game mode:
+    /// Check input format
+    /// Check disc type
+    /// Check lane number
+    /// Extract lane number
+    /// </summary>
+    /// <param name="input"></param>
+    /// <param name="lane"></param>
+    /// <returns></returns>
     public override bool TryParseMove(string input, out int lane)
     {
         lane = 0;
-        // check if the input is valid for a move
-        // check if the type is allowed for this game mode
-        // check if lane numbers are within reason. use orientation
-        // extract and IntParse lane.
-        // print errors as necessary
-        return false;
+        if (input[0] != 'o')
+        {
+            io.PrintError("Invalid disc type");
+            return false;
+        }
+
+        if (input.Length == 0 || input.Length > 2)
+        {
+            io.PrintError("Invalid input");
+            return false;
+        }
+
+        if (!int.TryParse(input.Substring(1), out lane))
+        {
+            // Parse failed
+            io.PrintError("Invalid Lane - Must be a number");
+            return false;
+        }
+        else
+        {
+            if (lane < 1 || lane > Grid.Board[1].Length)
+            {
+                io.PrintError("Invalid lane");
+            }
+
+            // Valid Input
+            return true;
+        }
     }
 
     public override bool PlayTurn(Computer player)
@@ -64,14 +97,20 @@ public class LineUpBasic : Game {
                 break;
             }
 
-            if (Grid.TurnCounter % 2 == 0)
+            // Holds the current player, based on turn number
+            Player activePlayer = Grid.TurnCounter % 2 == 1 ? PlayerOne : PlayerTwo;
+
+            // NOT IDEAL
+            // For true polymorphism, PlayTurn needs to exist on the Player object. 
+            // Which would mean the entire Game object also needs to be passed in...
+            bool success = PlayerOne switch
             {
-                PlayerOne.PlayTurn();
-            }
-            else
-            {
-                PlayerTwo.PlayTurn();
-            }
+                Human h => PlayTurn(h),
+                Computer c => PlayTurn(c),
+                _ => throw new ArgumentException("Unknown player type")
+            };
+                // PlayTurn(PlayerOne);
+
         }
     }
 }
