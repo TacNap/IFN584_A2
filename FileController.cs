4bd46e4323883436598588e02e5db4b82c688850
TacNap28 <matthewtlittle@gmail.com>
2025-10-17 09:29:21 +1000

    initial implementation of Serialization

--- FileController.cs
+++ FileController.cs
@@ -1,10 +1,54 @@
+using Newtonsoft.Json;
 public class FileController
 {
-    public void GridSerialization()
+    /// Uses the NewtonSoft JSON package to perform Serialization / Deserialization.
+    /// https://www.newtonsoft.com/json
+
+
+    /// <summary>
+    /// 
+    /// </summary>
+    /// <param name="game"></param>
+    public void GridSerialization(Game game)
     {
-        Console.WriteLine("[Run]\t FileController | GridSerialization");
+        // Check if directory exists
+        string saveDirectory = "Saves";
+        if (!Path.Exists(saveDirectory))
+        {
+            try
+            {
+                Directory.CreateDirectory(saveDirectory);
+            }
+            catch (Exception e)
+            {
+                IOController.PrintError($"Error: unable to create Saves directory, {e.Message}");
+                return;
+            }
+        }
+
+        // Create filename
+        string timestamp = DateTime.Now.ToString("yyyMMdd_HHmmss");
+        string fileName = $"Game_{timestamp}.json";
+        string filePath = Path.Combine(saveDirectory, fileName);
+
+        // Content to write to file
+        string json = JsonConvert.SerializeObject(game, Formatting.Indented, new JsonSerializerSettings
+        {
+            TypeNameHandling = TypeNameHandling.All
+        });
+
+        try
+        {
+            // Write to file
+            File.WriteAllText(filePath, json);
+            IOController.PrintSuccess($"Game saved to {filePath}");
+        }
+        catch (Exception e)
+        {
+            IOController.PrintError($"Error: Unable to write to save directory, {e.Message}");
+        }
     }
-    
+
     public void GridDeserialization()
     {
         Console.WriteLine("[Run]\t FileController | GridDeserialization");
