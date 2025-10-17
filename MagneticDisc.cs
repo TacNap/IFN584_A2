92c58ee267cbf199dae94e94d04a2104ebb504dc
Tyler <trieuhaipham32621998@gmail.com>
2025-10-17 11:47:23 +1000

    [fix] [disc] Magnetic: fixed

--- MagneticDisc.cs
+++ MagneticDisc.cs
@@ -11,31 +11,32 @@ public class MagneticDisc : Disc
 	public override bool ApplyEffects(ref Disc?[][] Board, int lane)
 	{
 		Console.WriteLine("[Run]\t MagneticDisc, ApplyEffects");
+		int laneIndex = lane - 1;
 
 		// Loop through played lane from the top
 		for (int i = 0; i < Board.Length; i++)
 		{
-			if (Board[i][lane] == null) continue;
-			Disc? d = Board[i][lane];
+			if (Board[i][laneIndex] == null) continue;
+			Disc? d = Board[i][laneIndex];
 
 			// Check if disc = symbol
 			if (d.Symbol == this.Symbol)
 			{
-				for (int j = i - 2; j >= 0; j--)
+				for (int j = i + 2; j < Board.Length; j++)
 				{
 					// Find the nearest ally in lane
-					if (Board[j][lane].IsPlayerOne == IsPlayerOne)
+					if (Board[j][laneIndex].IsPlayerOne == IsPlayerOne)
 					{
 						// Swap place with disc right above
-						Disc TempDisc = Board[j][lane];
-						Board[j][lane] = Board[j + 1][lane];
-						Board[j + 1][lane] = TempDisc;
+						Disc TempDisc = Board[j][laneIndex];
+						Board[j][laneIndex] = Board[j - 1][laneIndex];
+						Board[j - 1][laneIndex] = TempDisc;
 						break;
 					}
 				}
 
 				// Convert special disc into ordinary
-				Board[i][lane] = new OrdinaryDisc(IsPlayerOne);
+				Board[i][laneIndex] = new OrdinaryDisc(IsPlayerOne);
 			}
 		}
 
