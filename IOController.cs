453f16162277bface04e2a74b13d67988bd634b7
Tyler <trieuhaipham32621998@gmail.com>
2025-10-19 15:25:01 +1000

    [add] [io] TestMode; add test mode

--- IOController.cs
+++ IOController.cs
@@ -579,5 +579,154 @@ namespace LineUp2
             }
             return config;
         }
+        public static GameConfig GetInputTestMode()
+        {
+            PrintBannerNewGame();
+            GameConfig config = new GameConfig();
+
+            // ## Get Game Mode 
+            while (true)
+            {
+                // Get Input
+                PrintGreen("Which Game Mode?\n");
+                PrintGreen("[1] Classic\n");
+                PrintGreen("[2] Basic\n");
+                PrintGreen("[3] Spin\n");
+                Console.Write("> ");
+                string? input = Console.ReadLine();
+                int num;
+
+                // Validate it
+                bool success = int.TryParse(input, out num);
+                if (success)
+                {
+                    if (num < 1 || num > 3)
+                    {
+                        PrintError("Error: Invalid Choice");
+                        continue;
+                    }
+
+                    // Convert into GameMode value
+                    GameConfig.GameMode mode = GameConfig.GameMode.Classic;
+                    switch (num)
+                    {
+                        case 1:
+                            mode = GameConfig.GameMode.Classic;
+                            break;
+                        case 2:
+                            mode = GameConfig.GameMode.Basic;
+                            break;
+                        case 3:
+                            mode = GameConfig.GameMode.Spin;
+                            break;
+                    }
+
+                    // GameConfig value gets set here
+                    if (!config.SetGameMode(mode))
+                    {
+                        PrintError("Error: Unsupported Game Mode");
+                        continue;
+                    }
+                    break;
+                }
+                else
+                {
+                    PrintError("Error: Must be a number");
+                }
+            }
+
+            // Get Grid Size, if game mode is Classic
+            if (config.SelectedGameMode == GameConfig.GameMode.Classic)
+            {
+                // Check if they want to deviate from default
+                bool ChangeGrid = false;
+                while (true)
+                {
+                    // Get Input
+                    PrintGreen("Change Grid Size? [ Default : 6 x 7 ]\n");
+                    PrintGreen("[1] Yes\n");
+                    PrintGreen("[2] No\n");
+                    Console.Write("> ");
+                    string? input = Console.ReadLine();
+                    int num;
+
+                    // Validate it
+                    bool success = int.TryParse(input, out num);
+                    if (success)
+                    {
+                        if (num < 1 || num > 2)
+                        {
+                            PrintError("Error: Invalid Choice");
+                            continue;
+                        }
+                        if (num == 1)
+                        {
+                            ChangeGrid = true;
+                            break;
+                        }
+                        break;
+                    }
+                    else
+                    {
+                        PrintError("Error: Must be a number");
+                    }
+                }
+
+                // If yes, get dimensions
+                if (ChangeGrid)
+                {
+                    while (true)
+                    {
+                        // Get Input
+                        PrintGreen("Enter Rows [Min 1, Max 10]:\n");
+                        Console.Write("> ");
+                        string? input = Console.ReadLine();
+                        int num;
+
+                        // Validate it
+                        bool success = int.TryParse(input, out num);
+                        if (success)
+                        {
+                            if (!config.SetGridHeight(num))
+                            {
+                                PrintError("Error: Invalid value");
+                                continue;
+                            }
+                            break;
+                        }
+                        else
+                        {
+                            PrintError("Error: Must be a number");
+                        }
+                    }
+
+                    while (true)
+                    {
+                        // Get Input
+                        PrintGreen($"Enter Columns [Min {config.GridHeight}, Max 10]:\n");
+                        Console.Write("> ");
+                        string? input = Console.ReadLine();
+                        int num;
+
+                        // Validate it
+                        bool success = int.TryParse(input, out num);
+                        if (success)
+                        {
+                            if (!config.SetGridWidth(num))
+                            {
+                                PrintError("Error: Invalid value");
+                                continue;
+                            }
+                            break;
+                        }
+                        else
+                        {
+                            PrintError("Error: Must be a number");
+                        }
+                    }
+                }
+            }
+            return config;
+        }
     }
 }
